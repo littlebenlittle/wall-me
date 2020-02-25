f0ff0d7748ee2739c80c5ebb2f2644afbb39de62
Ben Little <ben.little6@gmail.com>
2020-02-25 07:57:10 -0700

    Add minimal function to pass test

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 
 import base64
+import json
+
+def extract_content(event):
+    s = base64.b64decode(event['data']).decode('utf-8')
+    j = json.loads(s)
+    return j
+
 
 def wallme(event, context):
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-    print(pubsub_message)
+    return pubsub_message
 
