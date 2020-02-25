e0ddcf8162a47a9a0d40125625c82d3aab100c6b
Ben Little <ben.little6@gmail.com>
2020-02-25 16:37:58 -0700

    Add code to pass tests

--- main.py
+++ main.py
@@ -21,6 +21,33 @@ def validate(data):
         return 0
     return 1
 
+def get_token(uri):
+    from google.cloud import storage
+    import re
+    s = re.compile(r'gs://([a-zA-Z0-9]+)/(.+)')
+    m = s.match(uri)
+    client = storage.Client()
+    bucket = client.bucket(m.group(1))
+    blob   = bucket.blob(m.group(2))
+    content = blob.download_as_string()
+    data = json.loads(content)
+    from google.oauth2.credentials import Credentials
+    creds = Credentials(
+        token=data['token'],
+        refresh_token=data['refresh_token'],
+        token_uri=data['token_uri'],
+        client_secret=data['client_secret'],
+        client_id=data['client_id'],
+        scopes=data['scopes'],
+    )
+    return creds
+
+def get_gmail_client():
+    from googleapiclient.discovery import build
+    creds = get_token('gs://wallme/v1/token.json')
+    gmail = build('gmail', 'v1', credentials=creds)
+    return gmail
+
 def wallme(event, context):
     data = extract_content(event)
     if not validate(data):
