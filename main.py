fa5b22bceda9ae714611a5e0dd7768629d17bb1a
Ben Little <ben.little6@gmail.com>
2020-02-25 08:39:13 -0700

    Add minimal passing validate() fn

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 
+import os
 import base64
 import json
+import logging
 
 def extract_content(event):
     s = base64.b64decode(event['data']).decode('utf-8')
     j = json.loads(s)
     return j
 
+def validate(data):
+    missing  = []
+    expected = [ 'emailAddress', 'historyId' ]
+    for k in expected:
+        if k not in data:
+            missing.append(k)
+    if len(missing) > 0:
+        return 0 
+    if data['emailAddress'] != os.environ['USER_EMAIL']:
+        return 0
+    return 1
 
 def wallme(event, context):
-    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    data = extract_content(event)
+    validate(data)
+    historyId = data['historyId']
     return pubsub_message
 
