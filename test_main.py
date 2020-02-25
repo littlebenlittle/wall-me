f233c5b375bd0ce46299dc2ed763983c27eef714
Ben Little <ben.little6@gmail.com>
2020-02-25 10:03:40 -0700

    Rename check_env vars to USER_EMAIL for clarity

--- test_main.py
+++ test_main.py
@@ -1,15 +1,17 @@
 
 import pytest
 import base64
-from main import extract_content, validate
+from main import extract_content, validate, get_gmail_client
+from googleapiclient.discovery import Resource
 
 @pytest.fixture
-def check_env_vars():
+def USER_EMAIL():
     import os
     import re
     email_regex = re.compile(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$')
     assert 'USER_EMAIL' in os.environ
     assert re.match(email_regex, os.environ['USER_EMAIL'])
+    return os.environ['USER_EMAIL']
 
 def test_extract_content():
     '''We can decode base64 string received from pubsub'''
@@ -19,10 +21,10 @@ def test_extract_content():
     res = extract_content(event)
     assert res["emailAddress"] == "user@example.com"
 
-def test_validate(check_env_vars):
+def test_validate(USER_EMAIL):
     '''A valid data obj is validated'''
     data = {
-        'emailAddress': 'ben.little6@gmail.com',
+        'emailAddress': USER_EMAIL,
         'historyId': '123456789',
     }
     assert validate(data)
@@ -38,16 +40,20 @@ def test_validate_no_emailAddress():
 def test_validate_no_historyId():
     '''An data obj with no historyId key is nixed'''
     data = {
-        'emailAddress': 'ben.little6@gmail.com',
+        'emailAddress': USER_EMAIL,
         'butter': 'no',
     }
     assert not validate(data)
 
-def test_validate_wrong_emailAddress(check_env_vars):
+def test_validate_wrong_emailAddress(USER_EMAIL):
     '''An data obj with wrond emailAddress value is nixed'''
     data = {
         'emailAddress': 'testy.mctest@test.tests',
         'historyId': '123456789',
     }
     assert not validate(data)
+
+def test_get_gmail_client():
+    gmail = get_gmail_client()
+    assert isinstance(gmail, Resource)
 
